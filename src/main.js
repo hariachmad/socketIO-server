14e83155d4296b2f949222ebefba7489ddc18e60
hariachmad <hari08achmad@gmail.com>
2026-02-05 08:27:38 +0700

    050226

--- src/main.js
+++ src/main.js
@@ -12,12 +12,12 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", (socket) => {
-  console.log("Client connected:", socket.id);
 
-  socket.on("recordCommand", (msg) => {
-    console.log("Record command:", msg, " from :", socket.userId);
-    io.emit("recordCommand", msg);
-  });
+  const { userId } = socket.handshake.auth;
+
+  console.log("Client connected:", userId);
+
+  socket.join(userId);
 
   NO_ACK_EVENTS.forEach((eventName) => {
     socket.on(eventName, (msg) => {
@@ -29,57 +29,107 @@ io.on("connection", (socket) => {
   ACK_EVENTS.forEach((eventName) => {
     socket.on(eventName, (msg, ack) => {
       console.log(`${eventName} command:`, msg, "from:", socket.userId);
-      io.emit(eventName, msg, (ackFromClient) => {
-        console.log(`${eventName} ack:`, ackFromClient, "from:", socket.userId);
-        ack(ackFromClient);
-      });
+      io.emit(eventName, msg);
     });
   });
 
   //Costum ACK
 
-  socket.on("SCREEN_BRIGHTNESS_SET", (msg, ack) => {
-    console.log("SCREEN_BRIGHTNESS_SET command:", msg, "from:", socket.userId);
-    io.emit("SCREEN_BRIGHTNESS_SET", msg.level, (ackFromClient) => {
-      const result = { level: ackFromClient.level }
-      ack({ result });
-    });
+  //ROBOT_MODE
+  socket.on("LISTENING", (msg, ack) => {
+    console.log("LISTENING:", msg, "from:", socket.userId);
+    io.to("raspberry").emit("LISTENING", msg);
+    io.to("frontend").emit("LISTENING", msg);
+  })
+
+  socket.on("TALKING", (msg, ack) => {
+    console.log("TALKING:", msg, "from:", socket.userId);
+    io.to("raspberry").emit("TALKING", msg);
+    io.to("frontend").emit("TALKING", msg);
+  })
+
+  //VOLUME
+  socket.on("INCREASE_VOLUME", (msg) => {
+    console.log("INCREASE_VOLUME : ", msg);
+    io.emit("INCREASE_VOLUME", msg)
   });
 
-  socket.on("SCREEN_BRIGHTNESS_GET", (msg, ack) => {
-    console.log("SCREEN_BRIGHTNESS_GET command:", msg, "from:", socket.userId);
-    io.emit("SCREEN_BRIGHTNESS_GET", msg, (ackFromClient) => {
-      const result = { level: ackFromClient.level }
-      ack({ result });
-    });
+  socket.on("DECREASE_VOLUME", (msg) => {
+    console.log("DECREASE_VOLUME : ", msg);
+    io.emit("DECREASE_VOLUME", msg)
   });
 
   socket.on("VOLUME_SET", (msg, ack) => {
-    console.log("VOLUME_SET command:", msg, "from:", socket.userId);
-    io.emit("VOLUME_SET", msg.volume, (ackFromClient) => {
-      const result = { volume: ackFromClient.volume }
-      ack({ result });
-    });
-
+    console.log("VOLUME_SET:", msg, "from:", socket.userId);
+    io.to("raspberry").emit("VOLUME_SET", msg);
   });
 
+  socket.on("VOLUME_GET_ACK", (msg) => {
+    console.log("VOLUME_GET_ACK:", msg, "from:", socket.userId);
+    io.to("frontend").emit("VOLUME_GET", msg);
+  })
+
   socket.on("VOLUME_GET", (msg, ack) => {
-    console.log("VOLUME_GET command:", msg, "from:", socket.userId);
-    io.emit("VOLUME_GET", msg, (ackFromClient) => {
-      const result = { volume: ackFromClient.volume }
-      ack({ result });      
-    })
+    console.log("VOLUME_GET", msg, "from:", socket.userId);
+    io.to("raspberry").emit("VOLUME_GET", msg);
+  });
+
+  //BRIGHTNESS
+  socket.on("INCREASE_BRIGHTNESS", (msg) => {
+    console.log("INCREASE_BRIGHTNESS : ", msg);
+    io.emit("INCREASE_BRIGHTNESS", msg)
+  });
+
+  socket.on("DECREASE_BRIGHTNESS", (msg) => {
+    console.log("DECREASE_BRIGHTNESS : ", msg);
+    io.emit("DECREASE_BRIGHTNESS", msg)
+  });
+
+  socket.on("BRIGHTNESS_SET", (msg, ack) => {
+    console.log("BRIGHTNESS_SET:", msg, "from:", socket.userId);
+    io.to("raspberry").emit("BRIGHTNESS_SET", msg);
+  });
+
+  socket.on("BRIGHTNESS_GET_ACK", (msg) => {
+    console.log("BRIGHTNESS_GET_ACK:", msg, "from:", socket.userId);
+    io.to("frontend").emit("BRIGHTNESS_GET", msg);
+  })
+
+  socket.on("BRIGHTNESS_GET", (msg, ack) => {
+    console.log("BRIGHTNESS_GET:", msg, "from:", socket.userId);
+    io.to("raspberry").emit("BRIGHTNESS_GET", msg);
+  });
+
+  //WAKE-UP
+  socket.on("PING_DEVICE_UP", (msg, ack) => {
+    console.log("WAKE_UP:", msg, "from:", socket.userId);
+    io.to("raspberry").emit("PING_DEVICE_UP", msg);
+  });
+
+  socket.on("PING_DEVICE_UP_FRONTEND", (msg, ack) => {
+    console.log("PING_DEVICE_UP_FRONTEND:", msg, "from:", socket.userId);
+    io.to("frontend").emit("BRIGHTNESS_SET", msg);
+    io.to("frontend-ui").emit("PING_DEVICE_UP", msg);
+  });
+
+  //SLEEP
+  socket.on("SLEEP", (msg, ack) => {
+    console.log("SLEEP:", msg, "from:", socket.userId);
+    io.to("raspberry").emit("SLEEP", msg);
+  });
+
+  socket.on("SLEEP_FRONTEND", (msg, ack) => {
+    console.log("SLEEP_FRONTEND:", msg, "from:", socket.userId);
+    io.to("frontend").emit("BRIGHTNESS_SET", msg);
+    io.to("frontend-ui").emit("SLEEP", msg);
   });
 
   socket.on("DEVICE_READY", (msg, ack) => {
     console.log("DEVICE_READY command:", msg, "from:", socket.userId);
-    io.emit("DEVICE_READY", msg, (ackFromClient) => {
-      const result = { ack : true }
-      ack({ result });      
-    })
+    io.emit("DEVICE_READY", msg)
   });
 })
 
 httpServer.listen(3000, () =>
-  console.log("🚀 Socket.IO server aktif di port 3000")
+  console.log(" Socket.IO server aktif di port 3000")
 );
